95a0ad1133d88937b3a69eaadcf37b34c7c7de29
julianadev <juramos266@gmail.com>
2024-02-18 15:46:29 -0300

    commit final

--- main.py
+++ main.py
@@ -3,27 +3,41 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv('.env')
-url = "https://jsearch.p.rapidapi.com/search"
 
-profissao = input('Digite a vaga que você deseja buscar: ')
+def get_response(profissao):
+    url = "https://jsearch.p.rapidapi.com/search"
+    querystring = {"query": profissao, "page": "1", "num_pages": "2"}
+    headers = {
+        "X-RapidAPI-Key": os.getenv('API_KEY'),
+        "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
 
-querystring = {"query": profissao, "page": "1", "num_pages": "1"}
+    try:
+        if response.status_code == 200:
+            df = response.json()
+            return df
+        return None
+    except requests.ConnectionError as e:
+        print(f'Erro na solicitação da API: {response.status_code}')
 
-headers = {
-    "X-RapidAPI-Key": os.getenv('API_KEY'),
-    "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
-}
+def main():
+    profissao = input('Digite a vaga que você deseja buscar: ')
+    response_content = get_response(profissao)
+    try:
+        if response_content is not None:
+            parametro = response_content.get('parameters', None)
+            print(parametro)
+            vagas = response_content.get('data', None)
+            for chave in vagas:
+                print(100 * '-')
+                for key, valor in chave.items():
+                    print(f'{key} - {valor}')
+        return None
+    except Exception as e:
+        print('Ocorreu um erro: ', e)
 
-response = requests.get(url, headers=headers, params=querystring)
 
-if response.status_code == 200:
-    data = response.json()
-    parametro = data.get('parameters', None)
-    print(parametro)
-    vagas = data.get('data', None)
-    for chave in vagas:
-        print(100 * '-')
-        for key, valor in chave.items():
-            print(f'{key} - {valor}')
-else:
-    print(f'Erro: {response.status_code}')
+if __name__ == "__main__":
+    main()
+
